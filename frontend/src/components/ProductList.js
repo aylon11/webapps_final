7bc5a504103ab4025b480e348684fbec485a6e2b
Dana Feder <dafeder@deloitte.co.il>
2020-09-06 14:25:56 +0300

    progressing with the searchbar

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -4,20 +4,37 @@ import { storeProducts } from '../data';
 import { ProductConsumer } from '../context';
 import Product from './Product';
 
+
 export default class ProductList extends Component {
+
     state = {
         products: storeProducts,
-        query: "",
+        inputValue: ""
     };
 
-
     render() {
+
+        function productsFilterOnChange(event) {
+
+            this.setState({
+                inputValue: event.target.value,
+                products: filteredProducts
+            })
+            debugger
+        }
+
+        const filteredProducts =
+        this.state.products.filter(product => {
+            return product.title.toLowerCase().includes(this.state.inputValue.toLocaleLowerCase())
+        })
+
+
         return (
             <React.Fragment>
                 <div className="py-5">
                     <div className="container text-center text title">
                         <Title name="Our " title="Products" />
-                        <input className="search-bar" placeholder="Search for..." value={this.state.query} onChange={() => { }} />
+                        <input className="search-bar" placeholder="Search for..." value={this.state.inputValue} onChange={productsFilterOnChange.bind(this)} />
                         <div className="row">
                             <ProductConsumer>
                                 {value => {
